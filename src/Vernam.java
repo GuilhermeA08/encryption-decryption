341047eec4ec8da34c02b4e28984fcd6486f59ee
GuilhermeA08 <guilhermesaldanha08@gmail.com>
2022-03-19 15:01:53 -0300

    feat: create Vernam

---
+++ src/Vernam.java
@@ -0,0 +1,10 @@
+public class Vernam {
+
+  public static String encrypted(String message) {
+    return message;
+  }
+
+  public static String decrypted(String message) {
+    return message;
+  }
+}
