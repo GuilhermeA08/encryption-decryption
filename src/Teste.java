cd13149dedf04507030cac6091300f92c5be4278
GuilhermeA08 <guilhermesaldanha08@gmail.com>
2022-03-19 22:38:08 -0300

    feat: add Vernam cipher test

--- src/Teste.java
+++ src/Teste.java
@@ -1,11 +1,20 @@
 public class Teste {
 
   public static void main(String[] args) {
+    //setup inicial
     String string = "ola mundo";
     int key = 1;
+
+    //teste cifra de cesar
     string = Cesar.encryption(string, key);
     System.out.println(string);
     string = Cesar.decryption(string, key);
+    System.out.println(string);
+
+    //teste cifra vernam
+    string = Vernam.encryptedAndDecrypted(string, "key");
+    System.out.println(string);
+    string = Vernam.encryptedAndDecrypted(string, "key");
     System.out.println(string);
   }
 }
