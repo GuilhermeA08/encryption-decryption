217eaba6cc51edc2818bd9fc7bf7fc1f0755adcd
GuilhermeA08 <guilhermesaldanha08@gmail.com>
2022-03-21 20:17:13 -0300

    feat: RC4 decrypt and encrypt mode

---
+++ src/RC4.java
@@ -0,0 +1,31 @@
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import javax.crypto.*;
+
+public class RC4 {
+
+  public static String encrypted(String message, SecretKey key)
+    throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+    Cipher cifrador = Cipher.getInstance("RC4");
+    cifrador.init(Cipher.ENCRYPT_MODE, key);
+
+    byte[] bytesCifrados = cifrador.doFinal(message.getBytes());
+    String mensagemCifrada = Base64.getEncoder().encodeToString(bytesCifrados);
+
+    return mensagemCifrada;
+  }
+
+  public static String decrypted(String cipherMessage, SecretKey key)
+    throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+    Cipher cifrador = Cipher.getInstance("RC4");
+    cifrador.init(Cipher.DECRYPT_MODE, key);
+
+    byte[] bytesDecifrados = cifrador.doFinal(
+      Base64.getDecoder().decode(cipherMessage)
+    );
+    String mensagemDecifrada = new String(bytesDecifrados);
+
+    return mensagemDecifrada;
+  }
+}
--- src/Teste.java
+++ src/Teste.java
@@ -1,66 +1,88 @@
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-
+import java.util.Base64;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 
 public class Teste {
 
-	public static void main(String[] args) {
-		// setup inicial
-		String string = "OLA MUNDO";
-		int key = 1;
+  public static void main(String[] args)
+    throws NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+    // setup inicial
+    String string = "OLA MUNDO";
+    int key = 1;
 
-		// teste cifra de cesar
-		string = Cesar.encryption(string, key);
-		System.out.println(string);
-		string = Cesar.decryption(string, key);
-		System.out.println(string);
+    KeyGenerator keyGenerator = KeyGenerator.getInstance("RC4");
+    keyGenerator.init(128);
+    SecretKey secretKey = keyGenerator.generateKey();
+    String secretKeyString = Base64
+      .getEncoder()
+      .encodeToString(secretKey.getEncoded());
+    System.out.println("generated key = " + secretKeyString);
 
-		// teste cifra vigenere
-		String plaintext = "Atacar base sul";
-		String keyword = "Limao";
-		System.out.println("-------------------------------");
-		System.out.println("Mensagem aberta: " + plaintext);
-		System.out.println("Palavra-chave: " + keyword);
-		System.out.println("-------------------------------");
-		System.out.println("Operação: ");
-		System.out.print("Chave: ");
-		plaintext = Vigenere.encryption(plaintext, keyword);
-		System.out.println("Mensagem cifrada: " + plaintext);
-		System.out.println("-------------------------------");
-		System.out.print("Chave: ");
-		plaintext = Vigenere.decryption(plaintext, keyword);
-		System.out.println("Mensagem decifrada: " + plaintext);
-		System.out.println("-------------------------------");
+    System.out.println("RC4");
+    System.out.println(RC4jc.encrypted(string, secretKey));
+    String rc4 = RC4jc.encrypted(string, secretKey);
+    System.out.println(RC4jc.decrypted(rc4, secretKey));
+    System.out.println("");
 
-		// teste cifra vernam
-		string = Vernam.encryptedAndDecrypted(string, "key");
-		System.out.println(string);
-		string = Vernam.encryptedAndDecrypted(string, "key");
-		System.out.println(string);
-		
-		string = "Ola mundo";
-		System.out.println("-------------------------------");
-		try {
-			string = AES.encryption(string);
-		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException
-				| InvalidAlgorithmParameterException | IllegalBlockSizeException | BadPaddingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("Mensagem cifrada: " + string);
-		System.out.println("-------------------------------");
-		try {
-			string = AES.decryption(string);
-		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException
-				| InvalidAlgorithmParameterException | IllegalBlockSizeException | BadPaddingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("Mensagem decifrada: " + string);
+    // teste cifra vigenere
+    String plaintext = "Atacar base sul";
+    String keyword = "Limao";
+    System.out.println("-------------------------------");
+    System.out.println("Mensagem aberta: " + plaintext);
+    System.out.println("Palavra-chave: " + keyword);
+    System.out.println("-------------------------------");
+    System.out.println("Operação: ");
+    System.out.print("Chave: ");
+    plaintext = Vigenere.encryption(plaintext, keyword);
+    System.out.println("Mensagem cifrada: " + plaintext);
+    System.out.println("-------------------------------");
+    System.out.print("Chave: ");
+    plaintext = Vigenere.decryption(plaintext, keyword);
+    System.out.println("Mensagem decifrada: " + plaintext);
+    System.out.println("-------------------------------");
 
-	}
+    // teste cifra vernam
+    string = Vernam.encryptedAndDecrypted(string, "key");
+    System.out.println(string);
+    string = Vernam.encryptedAndDecrypted(string, "key");
+    System.out.println(string);
+
+    string = "Ola mundo";
+    System.out.println("-------------------------------");
+    try {
+      string = AES.encryption(string);
+    } catch (
+      InvalidKeyException
+      | NoSuchAlgorithmException
+      | NoSuchPaddingException
+      | InvalidAlgorithmParameterException
+      | IllegalBlockSizeException
+      | BadPaddingException e
+    ) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    System.out.println("Mensagem cifrada: " + string);
+    System.out.println("-------------------------------");
+    try {
+      string = AES.decryption(string);
+    } catch (
+      InvalidKeyException
+      | NoSuchAlgorithmException
+      | NoSuchPaddingException
+      | InvalidAlgorithmParameterException
+      | IllegalBlockSizeException
+      | BadPaddingException e
+    ) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    System.out.println("Mensagem decifrada: " + string);
+  }
 }
