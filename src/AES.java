575940d037b06e231b6a8f5046f3f9188b8e16f4
Paulo Victor <paulovictorbema@gmail.com>
2022-03-21 20:03:30 -0300

    changes in AES

--- src/AES.java
+++ src/AES.java
@@ -7,6 +7,46 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 public class AES {
+	
+	static KeyGenerator keyGenerator;
+	static SecretKey secretKey;
+	static IvParameterSpec parameterSpec;
+	
+	public static String encryption(String plaintext) throws NoSuchAlgorithmException, NoSuchPaddingException,
+	InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		
+		keyGenerator = KeyGenerator.getInstance("AES");
+		keyGenerator.init(256);
+		secretKey = keyGenerator.generateKey();
+		String secretKeyString = Base64.getEncoder().encodeToString(secretKey.getEncoded());
+		System.out.println("Chave gerada = " + secretKeyString);
+		
+		Cipher encryptionCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		parameterSpec = new IvParameterSpec(new byte[16]);
+		encryptionCipher.init(Cipher.ENCRYPT_MODE, secretKey, parameterSpec);
+		byte[] encryptedMessageBytes = encryptionCipher.doFinal(plaintext.getBytes());
+		String encryptedMessage = Base64.getEncoder().encodeToString(encryptedMessageBytes);
+
+		return encryptedMessage;
+	}
+	
+	public static String decryption(String cryptedtext) throws NoSuchAlgorithmException, NoSuchPaddingException,
+	InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		
+		keyGenerator = KeyGenerator.getInstance("AES");
+		keyGenerator.init(256);
+		secretKey = keyGenerator.generateKey();
+		String secretKeyString = Base64.getEncoder().encodeToString(secretKey.getEncoded());
+		System.out.println("Chave gerada = " + secretKeyString);
+		
+		Cipher decryptionCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		decryptionCipher.init(Cipher.DECRYPT_MODE, secretKey, parameterSpec);
+		byte[] decryptedMessageBytes = decryptionCipher.doFinal();
+		String decryptedMessage = new String(decryptedMessageBytes);
+		
+		return decryptedMessage;
+	}
+	
 	public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException,
 			InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 		KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
--- src/Teste.java
+++ src/Teste.java
@@ -1,3 +1,11 @@
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
 public class Teste {
 
 	public static void main(String[] args) {
@@ -18,7 +26,7 @@ public class Teste {
 		System.out.println("Mensagem aberta: " + plaintext);
 		System.out.println("Palavra-chave: " + keyword);
 		System.out.println("-------------------------------");
-		System.out.println("Operação: ");
+		System.out.println("OperaÃ§Ã£o: ");
 		System.out.print("Chave: ");
 		plaintext = Vigenere.encryption(plaintext, keyword);
 		System.out.println("Mensagem cifrada: " + plaintext);
@@ -33,6 +41,26 @@ public class Teste {
 		System.out.println(string);
 		string = Vernam.encryptedAndDecrypted(string, "key");
 		System.out.println(string);
+		
+		string = "Ola mundo";
+		System.out.println("-------------------------------");
+		try {
+			string = AES.encryption(string);
+		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException
+				| InvalidAlgorithmParameterException | IllegalBlockSizeException | BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Mensagem cifrada: " + string);
+		System.out.println("-------------------------------");
+		try {
+			string = AES.decryption(string);
+		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException
+				| InvalidAlgorithmParameterException | IllegalBlockSizeException | BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Mensagem decifrada: " + string);
 
 	}
 }
